5c75e5fb8269033fd6618ed4736a9e0d53982996
raf4el12 <gomerorafael121@gmail.com>
2025-10-31 00:40:20 -0500

    fix: carrito update

--- server/src/modules/carrito/carrito.routes.js
+++ server/src/modules/carrito/carrito.routes.js
@@ -3,12 +3,10 @@ import * as carritoController from './carrito.controller.js'
 
 const router = express.Router()
 
-router.get('/', carritoController.getCarrito.bind(carritoController))
-router.get('/:id', carritoController.getCarritoById.bind(carritoController))
+router.get('/cliente/:id_cliente', carritoController.getCarritoByClienteId.bind(carritoController))
 router.post('/', carritoController.createCarrito.bind(carritoController))
 router.put('/:id', carritoController.updateCarrito.bind(carritoController))
 router.delete('/:id', carritoController.deleteCarrito.bind(carritoController))
 router.delete('/clear/:id_cliente', carritoController.clearCarritoByClienteId.bind(carritoController))
-router.get('/cliente/:id_cliente', carritoController.getCarritoByClienteId.bind(carritoController))
 
 export default router
