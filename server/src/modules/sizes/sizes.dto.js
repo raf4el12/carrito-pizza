4be2a86906c5234588479ebafbce545a33bf14ed
raf4el12 <gomerorafael121@gmail.com>
2025-11-16 17:56:47 -0500

    fix: sizes new column

--- server/src/modules/sizes/sizes.dto.js
+++ server/src/modules/sizes/sizes.dto.js
@@ -2,10 +2,14 @@ export const SizeDTO = {
   id_tamano: true,
   nombre: true,
   descripcion: true,
+  precio_base: true,
+  activo: true,
 }
 
 export const SizeListDTO = {
   id_tamano: true,
   nombre: true,
+  precio_base: true,
+  activo: true,
 }
 
